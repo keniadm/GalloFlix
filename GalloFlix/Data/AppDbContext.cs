b3948fb2105c1319135150b8cff00d8e84f05ecf
Kênia De Marchi <keniademarchi@gmail.com>
2023-05-02 12:29:28 -0300

    Finalização da classe Context

--- GalloFlix/Data/AppDbContext.cs
+++ GalloFlix/Data/AppDbContext.cs
@@ -22,6 +22,7 @@ public class AppDbContext : IdentityDbContext
         base.OnModelCreating(builder);
 
         //FluentAPI
+        #region Personalização do Identity
         builder.Entity<IdentityUser>(b => {
             b.ToTable("Users");
         });
@@ -43,5 +44,51 @@ public class AppDbContext : IdentityDbContext
         builder.Entity<IdentityUserRole<string>>(b => {
             b.ToTable("UserRoles");
         });
+        #endregion
+
+        #region Many To Many - MovieComment
+        builder.Entity<MovieComment>()
+            .HasOne(mc => mc.Movie)
+            .WithMany(m => m.Comments)
+            .HasForeignKey(mc => mc.MovieId);
+
+        builder.Entity<MovieComment>()
+            .HasOne(mc => mc.User)
+            .WithMany(u => u.Comments)
+            .HasForeignKey(mc => mc.UserId);
+        #endregion
+
+        #region Many To Many - MovieGenre
+        // Definição de Chave Primária Composta
+        builder.Entity<MovieGenre>().HasKey(
+            mg => new { mg.MovieId, mg.GenreId }
+        );
+
+        builder.Entity<MovieGenre>()
+            .HasOne(mg => mg.Movie)
+            .WithMany(m => m.Genres)
+            .HasForeignKey(mg => mg.MovieId);
+
+        builder.Entity<MovieGenre>()
+            .HasOne(mg => mg.Genre)
+            .WithMany(g => g.Movies)
+            .HasForeignKey(mg => mg.GenreId);
+        #endregion
+
+        #region Many To Many - MovieRating
+        builder.Entity<MovieRating>().HasKey(
+            mr => new { mr.MovieId, mr.UserId }
+        );
+
+        builder.Entity<MovieRating>()
+            .HasOne(mr => mr.Movie)
+            .WithMany(m => m.Ratings)
+            .HasForeignKey(mr => mr.MovieId);
+
+        builder.Entity<MovieRating>()
+            .HasOne(mr => mr.User)
+            .WithMany(u => u.Ratings)
+            .HasForeignKey(mr => mr.UserId);
+        #endregion
     }
 }
