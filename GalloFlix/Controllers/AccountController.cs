fd1fe3a803bb1f2f666b5b290aaef03dc12e7be9
Kênia De Marchi <keniademarchi@gmail.com>
2023-06-05 11:34:12 -0300

    Criação do serviço de email e registro

--- GalloFlix/Controllers/AccountController.cs
+++ GalloFlix/Controllers/AccountController.cs
@@ -1,10 +1,13 @@
 using System.Net.Mail;
 using System.Security.Claims;
+using System.Text;
 using GalloFlix.DataTransferObjects;
 using GalloFlix.Models;
+using GalloFlix.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
 
 namespace GalloFlix.Controllers;
 
@@ -14,14 +17,23 @@ public class AccountController : Controller
     private readonly ILogger<AccountController> _logger;
     private readonly SignInManager<AppUser> _signInManager;
     private readonly UserManager<AppUser> _userManager;
+    private readonly IUserStore<AppUser> _userStore;
+    private readonly IUserEmailStore<AppUser> _emailStore;
+    private readonly IEmailSender _emailSender;
 
     public AccountController(ILogger<AccountController> logger,
         SignInManager<AppUser> signInManager,
-        UserManager<AppUser> userManager)
+        UserManager<AppUser> userManager,
+        IUserStore<AppUser> userStore,
+        IEmailSender emailSender
+    )
     {
         _logger = logger;
         _signInManager = signInManager;
         _userManager = userManager;
+        _userStore = userStore;
+        _emailStore = (IUserEmailStore<AppUser>)_userStore;
+        _emailSender = emailSender;
     }
 
     public IActionResult Index()
@@ -92,7 +104,31 @@ public class AccountController : Controller
     {
         if (ModelState.IsValid) 
         {
+            var user = Activator.CreateInstance<AppUser>();
+
+            user.Name = register.Name;
+            user.DateOfBirth = register.DateOfBirth;
+            user.Email = register.Email;
+
+            await _userStore.SetUserNameAsync(
+                user, register.Email, CancellationToken.None);
+            await _emailStore.SetEmailAsync(
+                user, register.Email, CancellationToken.None);
             
+            var result = await _userManager.CreateAsync(user, register.Password);
+
+            if(result.Succeeded)
+            {
+                _logger.LogInformation($"Novo usuário registrado com o email {user.Email}");
+
+                var userId = await _userManager.GetUserIdAsync(user);
+                var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                var callbackUrl = Url.Action(
+                    "ConfirmEmail", "Account", new { userId = userId, code = code},
+                    protocol: Request.Scheme
+                );
+            }
         }
         return View(register);
     }
--- GalloFlix/Services/EmailSender.cs
+++ GalloFlix/Services/EmailSender.cs
@@ -1,8 +1,30 @@
+using System.Net;
+using System.Net.Mail;
+
 namespace GalloFlix.Services;
 public class EmailSender : IEmailSender
 {
-    public Task SendEmailAsync(string email, string subject, string htmlMessage)
+    public EmailSender()
     {
-        throw new NotImplementedException();
+    }
+
+    public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+    {
+        string fromMail = "gallozord@outlook.com";
+        string fromPassword = "@Etec123#";
+
+        MailMessage message = new();
+        message.From = new MailAddress(fromMail);
+        message.Subject = subject;
+        message.To.Add(new MailAddress(email));
+        message.Body = "<html><body>" + htmlMessage + "</body></html>";
+        message.IsBodyHtml = true;
+        var smtpClient = new SmtpClient("smtp-mail.outlook.com") 
+        {
+            Port = 587,
+            Credentials = new NetworkCredential(fromMail, fromPassword),
+            EnableSsl = true
+        };
+        await smtpClient.SendMailAsync(message);
     }
 }
